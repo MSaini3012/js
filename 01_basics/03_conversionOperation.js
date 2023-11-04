de7a2efac9fb55c589a7f6d390dd28cef4991a07
MSaini3012 <149887034+MSaini3012@users.noreply.github.com>
2023-11-04 17:18:00 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+// let score = "Mohit";
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+
+// console.log(valueInNumber)
+
+let loggdIn = 1;
+
+let booleanIsLoggedIn = Boolean(loggdIn)
+
+console.log(booleanIsLoggedIn);
+
+// "" => false , "Mohit" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof(stringNumber));
